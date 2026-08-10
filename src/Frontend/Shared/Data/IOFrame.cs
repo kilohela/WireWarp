3729cfe80c24380f3d13d597d8a4b3f1249cecf0
yfdyzjt <yfdyzjt@gmail.com>
2026-08-10 12:13:12 +0800

    Make io frame static

--- src/Frontend/Shared/Data/IOFrame.cs
+++ src/Frontend/Shared/Data/IOFrame.cs
@@ -1,27 +1,24 @@
 namespace WireWarp.Frontend.Shared.Data;
 
-public class IOFrame(WiringGraph graph)
+public static class IOFrame
 {
-    public int InputPortCount { get; } = graph.InputPorts.Count;
-    public int OutputPortCount { get; } = graph.OutputPorts.Count;
+    private static readonly List<int>[] _inputBuffers = [[], []];
+    private static readonly List<int>[] _outputBuffers = [[], []];
+    private static long _tick;
 
-    private readonly List<int>[] _inputBuffers = [[], []];
-    private readonly List<int>[] _outputBuffers = [[], []];
-    private long _tick;
+    public static void AddInput(int portId) => _inputBuffers[_tick & 1].Add(portId);
+    public static void AddOutput(int portId) => _outputBuffers[_tick & 1].Add(portId);
+    public static IReadOnlyList<int> ReadInputs() => _inputBuffers[(_tick + 1) & 1];
+    public static IReadOnlyList<int> ReadOutputs() => _outputBuffers[(_tick + 1) & 1];
 
-    public void AddInput(int portId) => _inputBuffers[_tick & 1].Add(portId);
-    public void AddOutput(int portId) => _outputBuffers[_tick & 1].Add(portId);
-    public IReadOnlyList<int> ReadInputs() => _inputBuffers[(_tick + 1) & 1];
-    public IReadOnlyList<int> ReadOutputs() => _outputBuffers[(_tick + 1) & 1];
-
-    public void Tick()
+    public static void Tick()
     {
         _tick++;
         _inputBuffers[_tick & 1].Clear();
         _outputBuffers[_tick & 1].Clear();
     }
 
-    public void Reset()
+    public static void Reset()
     {
         _tick = 0;
         _inputBuffers[0].Clear();
