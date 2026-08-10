43c349c8ea093dae503f288f94681e854ef8dec8
yfdyzjt <yfdyzjt@gmail.com>
2026-08-11 06:43:41 +0800

    Remove tile hash

--- src/Frontend/Shared/File/WiringHash.cs
+++ src/Frontend/Shared/File/WiringHash.cs
@@ -6,33 +6,10 @@ namespace WireWarp.Frontend.Shared.File;
 
 public static class WiringHash
 {
-    public const int TileCellSize = 7;
-
-    public static void GetTileHash(Span<byte> dst, Tile tile)
-    {
-        byte flags = 0;
-
-        if (tile.HasTile) flags |= 1 << 0;
-
-        if (tile.HasActuator) flags |= 1 << 1;
-        if (tile.IsActuated) flags |= 1 << 2;
-
-        if (tile.RedWire) flags |= 1 << 3;
-        if (tile.BlueWire) flags |= 1 << 4;
-        if (tile.GreenWire) flags |= 1 << 5;
-        if (tile.YellowWire) flags |= 1 << 6;
-
-        dst[0] = flags;
-        BinaryPrimitives.WriteUInt16LittleEndian(dst[1..], tile.type);
-        BinaryPrimitives.WriteInt16LittleEndian(dst[3..], tile.frameX);
-        BinaryPrimitives.WriteInt16LittleEndian(dst[5..], tile.frameY);
-    }
-
     public static byte[] GetWiringHash()
     {
         using var hash = IncrementalHash.CreateHash(HashAlgorithmName.SHA256);
-        Span<byte> pos = stackalloc byte[8];
-        Span<byte> cell = stackalloc byte[TileCellSize];
+        Span<byte> cell = stackalloc byte[15];
 
         for (var y = 0; y < Main.maxTilesY; y++)
         {
@@ -41,11 +18,23 @@ public static class WiringHash
                 var tile = Main.tile[x, y];
                 if (!Detector.HasWiring(tile)) continue;
 
-                BinaryPrimitives.WriteInt32LittleEndian(pos, x);
-                BinaryPrimitives.WriteInt32LittleEndian(pos[4..], y);
-                hash.AppendData(pos);
+                byte flags = 0;
 
-                GetTileHash(cell, tile);
+                if (tile.HasTile) flags |= 1 << 0;
+                if (tile.HasActuator) flags |= 1 << 1;
+                if (tile.IsActuated) flags |= 1 << 2;
+                if (tile.RedWire) flags |= 1 << 3;
+                if (tile.BlueWire) flags |= 1 << 4;
+                if (tile.GreenWire) flags |= 1 << 5;
+                if (tile.YellowWire) flags |= 1 << 6;
+
+                cell[0] = flags;
+                BinaryPrimitives.WriteInt32LittleEndian(cell[1..], x);
+                BinaryPrimitives.WriteInt32LittleEndian(cell[5..], y);
+                BinaryPrimitives.WriteUInt16LittleEndian(cell[9..], tile.type);
+                BinaryPrimitives.WriteInt16LittleEndian(cell[11..], tile.frameX);
+                BinaryPrimitives.WriteInt16LittleEndian(cell[13..], tile.frameY);
+
                 hash.AppendData(cell);
             }
         }
