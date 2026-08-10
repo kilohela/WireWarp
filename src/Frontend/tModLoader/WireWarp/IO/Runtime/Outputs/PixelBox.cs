50d12faa1390ca323db781cb6281448636a3137d
yfdyzjt <yfdyzjt@gmail.com>
2026-08-10 16:27:41 +0800

    Add output pixel box logic

--- src/Frontend/tModLoader/WireWarp/IO/Runtime/Outputs/PixelBox.cs
+++ src/Frontend/tModLoader/WireWarp/IO/Runtime/Outputs/PixelBox.cs
@@ -1,6 +1,13 @@
+using Terraria;
+
 namespace WireWarp.Frontend.tModLoader.IO;
 
 partial class RuntimeOutput
 {
-    private static void PixelBox(int i, int j) { }
+    private static void PixelBox(int i, int j)
+    {
+        Tile tile = Main.tile[i, j];
+        tile.TileFrameX = (short)((tile.TileFrameX != 18) ? 18 : 0);
+        NetMessage.SendTileSquare(-1, i, j);
+    }
 }
